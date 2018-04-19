1bac05c546e7b0dbd9cdd7803453d6756517aa22
vivianmahr <vivianmahr@gmail.com>
2018-04-19 04:47:05 -0700

    Spacy fix 1

--- c_sea_dir/views.py
+++ c_sea_dir/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
-import spacy
-nlp = spacy.load('en')
+import en_core_web_sm
+nlp= en_core_web_sm.load()
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
+    
     r = requests.get('http://httpbin.org/status/418')
+    print(nlp)
     print(r.text)
-    print("spacy has loaded")
     return HttpResponse('<pre>' + r.text + 'loaded!</pre>')
 
 def db(request):
